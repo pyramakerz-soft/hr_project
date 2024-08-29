4350b5778d90e7778324dfc0b1bbcf4ae64e2061
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-08-29 14:31:11 +0300

    Make sure that naming according to camelCase

--- lib/features/dashboard/data/remote_data_source/remote_data_source.dart
+++ lib/features/dashboard/data/remote_data_source/remote_data_source.dart
@@ -5,24 +5,17 @@ import 'package:pyramakerz_atendnace/features/dashboard/data/models/clock_models
 import 'package:pyramakerz_atendnace/features/dashboard/data/models/clock_models/clock_response.dart';
 
 abstract class BaseDashboardRemoteDataSource {
-
-
-Future<ClockResponse>  clockIn(ClockRequest request);
-Future<ClockResponse>  clockOut(ClockRequest request);
-
+  Future<ClockResponse> clockIn(ClockRequest request);
+  Future<ClockResponse> clockOut(ClockRequest request);
 }
-
 
 @injectable.Order(-3)
 @injectable.Singleton(as: BaseDashboardRemoteDataSource)
 class DashboardRemoteDataSource extends BaseDashboardRemoteDataSource {
   @override
-  Future<ClockResponse> clockIn(ClockRequest request) async{
-    final response = await ApiHelper().post(
-      path:
-          ApiConstants.clockin,
-          body: request.toMap()
-    );
+  Future<ClockResponse> clockIn(ClockRequest request) async {
+    final response = await ApiHelper()
+        .post(path: ApiConstants.clockIn, body: request.toMap());
 
     return response.fold(
       (left) => throw left,
@@ -31,17 +24,13 @@ class DashboardRemoteDataSource extends BaseDashboardRemoteDataSource {
   }
 
   @override
-  Future<ClockResponse> clockOut(ClockRequest request)async {
-   final response = await ApiHelper().post(
-      path:
-          ApiConstants.clockout,
-          body: request.toMap()
-    );
+  Future<ClockResponse> clockOut(ClockRequest request) async {
+    final response = await ApiHelper()
+        .post(path: ApiConstants.clockOut, body: request.toMap());
 
     return response.fold(
       (left) => throw left,
       (right) => ClockResponse.fromMap(right.data),
     );
   }
-
 }
