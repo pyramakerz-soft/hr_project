5dc3a8307cf0dca912366fb6446ba78cbfdc3116
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-09-01 02:36:03 +0300

    Adding the home cubit

--- lib/my_blocs_provider.dart
+++ lib/my_blocs_provider.dart
@@ -1,12 +1,15 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pyramakerz_atendnace/features/auth/persentation/login/bloc/bloc/auth_bloc.dart';
 import 'package:pyramakerz_atendnace/core/di/dependency_config.dart';
+import 'package:pyramakerz_atendnace/features/home/presentation/cubit/home_cubit.dart';
 
 class MyBlockProviders {
   static getAllProvider() => [
-          BlocProvider(
+        BlocProvider(
           create: (context) => getIt<AuthBloc>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<HomeCubit>()..init(),
         ),
       ];
 }
