defbb37f7170d0a1511bb1096d60275819a149d5
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-09-02 15:03:37 +0300

    Adding the get all clocks method to the data layer

--- lib/features/home/data/data_source/home_remote_source.dart
+++ lib/features/home/data/data_source/home_remote_source.dart
@@ -2,12 +2,14 @@ import 'package:injectable/injectable.dart' as injectable;
 import 'package:pyramakerz_atendnace/core/constants/api_constants.dart';
 import 'package:pyramakerz_atendnace/core/dio/api_helper.dart';
 import 'package:pyramakerz_atendnace/core/error/failure.dart';
+import 'package:pyramakerz_atendnace/features/dashboard/data/models/clock_history/clock_history.dart';
 import 'package:pyramakerz_atendnace/features/dashboard/data/models/clock_models/clock_request.dart';
 import 'package:pyramakerz_atendnace/features/dashboard/data/models/clock_models/clock_response.dart';
 
 abstract class HomeRemoteSource {
   Future<Clock> checkIn({required ClockRequest request});
   Future<void> checkOut({required ClockRequest request});
+  Future<ClockHistoryResponse> getMyClocks({int page = 1});
 }
 
 @injectable.Order(-3)
@@ -38,6 +40,19 @@ class HomeRemoteSourceImpl implements HomeRemoteSource {
               message: e.errorMessageModel.message.toString(),
               statusCode: e.errorMessageModel.statusCode),
           (r) => Clock.fromMap(r.data));
+    });
+  }
+
+  @override
+  Future<ClockHistoryResponse> getMyClocks({int page = 1}) {
+    return _apiHelper
+        .get(path: "${ApiConstants.getMyClocks}?page=$page")
+        .then((response) {
+      return response.fold(
+          (e) => throw Failure(
+              message: e.errorMessageModel.message.toString(),
+              statusCode: e.errorMessageModel.statusCode),
+          (r) => ClockHistoryResponse.fromMap(r.data['data']));
     });
   }
 }
--- lib/features/home/presentation/cubit/home_state.dart
+++ lib/features/home/presentation/cubit/home_state.dart
@@ -13,11 +13,28 @@ enum CheckInStateStatus {
   checkedOut,
 }
 
+enum MyClocksStateStatus {
+  gettingClocks,
+  gettingMoreClocks,
+  gotClocks,
+  gettingClocksError,
+}
+
 extension HomeStateX on HomeState {
   bool get isInitial => status == HomeStateStatus.initial;
   bool get isLoading => status == HomeStateStatus.loading;
   bool get isLoaded => status == HomeStateStatus.loaded;
   bool get isError => status == HomeStateStatus.error;
+}
+
+extension MyClocksStateX on HomeState {
+  bool get isGettingClocks =>
+      myClocksStateStatus == MyClocksStateStatus.gettingClocks;
+  bool get isGotClocks => myClocksStateStatus == MyClocksStateStatus.gotClocks;
+  bool get isGettingMoreClocks =>
+      myClocksStateStatus == MyClocksStateStatus.gettingMoreClocks;
+  bool get isGettingClocksError =>
+      myClocksStateStatus == MyClocksStateStatus.gettingClocksError;
 }
 
 extension CheckInStateStatusX on HomeState {
@@ -28,29 +45,44 @@ extension CheckInStateStatusX on HomeState {
 class HomeState {
   final HomeStateStatus status;
   final CheckInStateStatus? checkInStatus;
+  final MyClocksStateStatus? myClocksStateStatus;
   final String? formattedAddress;
   final Position? currentLocation;
   final Clock? workingData;
   final String? error;
   final User? user;
+  final List<ClockHistory> myClocks;
+  final int currentPage;
+  final int? totalPages;
+  final bool? isLocationPermissionGranted;
   const HomeState({
     this.status = HomeStateStatus.initial,
+    this.myClocksStateStatus,
     this.checkInStatus,
     this.formattedAddress,
     this.workingData,
     this.currentLocation,
     this.user,
     this.error,
+    this.myClocks = const [],
+    this.currentPage = 1,
+    this.totalPages,
+    this.isLocationPermissionGranted,
   });
 
   HomeState copyWith({
     HomeStateStatus? status,
     CheckInStateStatus? checkInStatus,
     Clock? workingData,
+    MyClocksStateStatus? myClocksStateStatus,
     String? formattedAddress,
     Position? currentLocation,
+    List<ClockHistory>? myClocks,
     User? user,
     String? error,
+    int? currentPage,
+    int? totalPages,
+    bool? isLocationPermissionGranted,
   }) {
     return HomeState(
       status: status ?? this.status,
@@ -58,8 +90,14 @@ class HomeState {
       checkInStatus: checkInStatus ?? this.checkInStatus,
       currentLocation: currentLocation ?? this.currentLocation,
       user: user ?? this.user,
+      myClocksStateStatus: myClocksStateStatus ?? this.myClocksStateStatus,
       formattedAddress: formattedAddress ?? this.formattedAddress,
+      myClocks: myClocks ?? this.myClocks,
       error: error ?? this.error,
+      currentPage: currentPage ?? this.currentPage,
+      totalPages: totalPages ?? this.totalPages,
+      isLocationPermissionGranted:
+          isLocationPermissionGranted ?? this.isLocationPermissionGranted,
     );
   }
 
@@ -70,9 +108,14 @@ class HomeState {
     return other.status == status &&
         other.workingData == workingData &&
         other.user == user &&
+        other.myClocksStateStatus == myClocksStateStatus &&
         other.checkInStatus == checkInStatus &&
         other.formattedAddress == formattedAddress &&
         other.currentLocation == currentLocation &&
+        listEquals(other.myClocks, myClocks) &&
+        other.currentPage == currentPage &&
+        other.totalPages == totalPages &&
+        other.isLocationPermissionGranted == isLocationPermissionGranted &&
         other.error == error;
   }
 
@@ -83,6 +126,11 @@ class HomeState {
       checkInStatus.hashCode ^
       formattedAddress.hashCode ^
       user.hashCode ^
+      myClocksStateStatus.hashCode ^
       error.hashCode ^
+      Object.hashAll(myClocks) ^
+      currentPage.hashCode ^
+      totalPages.hashCode ^
+      isLocationPermissionGranted.hashCode ^
       currentLocation.hashCode;
 }
