b844ceaded7433297558cdc8591ebb6eaa7a5a7d
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-09-02 15:01:52 +0300

    create clock history and clock history response

---
+++ lib/features/dashboard/data/models/clock_history/clock_history.dart
@@ -0,0 +1,171 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class ClockHistoryResponse {
+  final List<ClockHistory>? clocks;
+  final Pagination? pagination;
+
+  ClockHistoryResponse({
+    this.clocks,
+    this.pagination,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'clocks': clocks?.map((x) => x.toMap()).toList(),
+      'pagination': pagination?.toMap(),
+    };
+  }
+
+  factory ClockHistoryResponse.fromMap(Map<String, dynamic> map) {
+    return ClockHistoryResponse(
+      clocks: map['clocks'] != null
+          ? List<ClockHistory>.from((map['clocks'] as List)
+              .map((x) => ClockHistory.fromMap(x as Map<String, dynamic>)))
+          : null,
+      pagination: map['pagination'] != null
+          ? Pagination.fromMap(map['pagination'] as Map<String, dynamic>)
+          : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ClockHistoryResponse.fromJson(String source) =>
+      ClockHistoryResponse.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+
+class Pagination {
+  final int? currentPage;
+  final String? nextPageUrl;
+  final String? previousPageUrl;
+  final int? lastPage;
+  final int? total;
+  Pagination({
+    this.currentPage,
+    this.nextPageUrl,
+    this.previousPageUrl,
+    this.lastPage,
+    this.total,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'current_page': currentPage,
+      'next_page_url': nextPageUrl,
+      'previous_page_url': previousPageUrl,
+      'last_page': lastPage,
+      'total': total,
+    };
+  }
+
+  factory Pagination.fromMap(Map<String, dynamic> map) {
+    return Pagination(
+      currentPage:
+          map['current_page'] != null ? map['current_page'] as int : null,
+      nextPageUrl:
+          map['next_page_url'] != null ? map['next_page_url'] as String : null,
+      previousPageUrl: map['previous_page_url'] != null
+          ? map['previous_page_url'] as String
+          : null,
+      lastPage: map['last_page'] != null ? map['last_page'] as int : null,
+      total: map['total'] != null ? map['total'] as int : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Pagination.fromJson(String source) =>
+      Pagination.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+
+class ClockHistory {
+  final int? id;
+  final String? day;
+  final String? date;
+  final String? clockIn;
+  final String? clockOut;
+  final String? totalHours;
+  final int? userId;
+  final String? site;
+  final String? formattedClockIn;
+  final String? formattedClockOut;
+  ClockHistory({
+    this.id,
+    this.day,
+    this.date,
+    this.clockIn,
+    this.clockOut,
+    this.totalHours,
+    this.userId,
+    this.site,
+    this.formattedClockIn,
+    this.formattedClockOut,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'Day': day,
+      'Date': date,
+      'clockIn': clockIn,
+      'clockOut': clockOut,
+      'totalHours': totalHours,
+      'userId': userId,
+      'site': site,
+      'formattedClockIn': formattedClockIn,
+      'formattedClockOut': formattedClockOut,
+    };
+  }
+
+  factory ClockHistory.fromMap(Map<String, dynamic> map) {
+    return ClockHistory(
+      id: map['id'] != null ? map['id'] as int : null,
+      day: map['Day'] != null ? map['Day'] as String : null,
+      date: map['Date'] != null ? map['Date'] as String : null,
+      clockIn: map['clockIn'] != null ? map['clockIn'] as String : null,
+      clockOut: map['clockOut'] != null ? map['clockOut'] as String : null,
+      totalHours:
+          map['totalHours'] != null ? map['totalHours'] as String : null,
+      userId: map['userId'] != null ? map['userId'] as int : null,
+      site: map['site'] != null ? map['site'] as String : null,
+      formattedClockIn: map['formattedClockIn'] != null
+          ? map['formattedClockIn'] as String
+          : null,
+      formattedClockOut: map['formattedClockOut'] != null
+          ? map['formattedClockOut'] as String
+          : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ClockHistory.fromJson(String source) =>
+      ClockHistory.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  ClockHistory copyWith({
+    int? id,
+    String? day,
+    String? date,
+    String? clockIn,
+    String? clockOut,
+    int? userId,
+    String? site,
+    String? totalHours,
+    String? formattedClockIn,
+    String? formattedClockOut,
+  }) {
+    return ClockHistory(
+      id: id ?? this.id,
+      day: day ?? this.day,
+      date: date ?? this.date,
+      clockIn: clockIn ?? this.clockIn,
+      clockOut: clockOut ?? this.clockOut,
+      userId: userId ?? this.userId,
+      site: site ?? this.site,
+      totalHours: totalHours ?? this.totalHours,
+      formattedClockIn: formattedClockIn ?? this.formattedClockIn,
+      formattedClockOut: formattedClockOut ?? this.formattedClockOut,
+    );
+  }
+}
