595b7bb24b5b05226da99faab58b94bde2918866
ZeyadShawki <zizoelfager14@gmail.com>
2024-09-04 09:32:01 +0300

    added no internet connection case in auth guard

--- lib/core/di/dependency_config.config.dart
+++ lib/core/di/dependency_config.config.dart
@@ -31,16 +31,6 @@ import 'package:pyramakerz_atendnace/features/auth/persentation/login/bloc/bloc/
     as _i13;
 import 'package:pyramakerz_atendnace/features/clock_in/presentation/cubit/clock_in_cubit.dart'
     as _i1067;
-import 'package:pyramakerz_atendnace/features/dashboard/data/remote_data_source/remote_data_source.dart'
-    as _i1059;
-import 'package:pyramakerz_atendnace/features/dashboard/data/repo/repo.dart'
-    as _i160;
-import 'package:pyramakerz_atendnace/features/dashboard/domain/base_repo/base_repo.dart'
-    as _i171;
-import 'package:pyramakerz_atendnace/features/dashboard/domain/usecases/clockin_usecase.dart'
-    as _i350;
-import 'package:pyramakerz_atendnace/features/dashboard/domain/usecases/clockout_usecase.dart'
-    as _i938;
 import 'package:pyramakerz_atendnace/features/home/data/data_source/home_remote_source.dart'
     as _i378;
 import 'package:pyramakerz_atendnace/features/home/data/repository/home_repository.dart'
@@ -68,37 +58,27 @@ extension GetItInjectableX on _i174.GetIt {
     gh.lazySingleton<_i640.ApiHelper>(() => _i640.ApiHelper());
     gh.factory<_i963.AppPreferences>(() =>
         _i963.AppPreferences(sharedPreferences: gh<_i460.SharedPreferences>()));
-    gh.singleton<_i1059.BaseDashboardRemoteDataSource>(
-        () => _i1059.DashboardRemoteDataSource());
     gh.singleton<_i615.NetworkInfo>(() => _i615.NetworkInfoImpl());
     gh.singleton<_i378.HomeRemoteSource>(
         () => _i378.HomeRemoteSourceImpl(apiHelper: gh<_i640.ApiHelper>()));
     gh.singleton<_i441.BaseAuthRemoteDataSource>(
         () => _i441.AuthRemoteDataSource(apiHelper: gh<_i640.ApiHelper>()));
     gh.singleton<_i984.LocationService>(() => _i984.LocationServiceImpl());
-    gh.singleton<_i171.BaseDashboardReposetry>(() => _i160.DashboardRepository(
-          gh<_i1059.BaseDashboardRemoteDataSource>(),
-          gh<_i615.NetworkInfo>(),
-        ));
     gh.singleton<_i233.HomeRepository>(() =>
         _i233.HomeRepositoryImpl(remoteSource: gh<_i378.HomeRemoteSource>()));
     gh.singleton<_i283.BaseAuthReposetry>(() => _i320.AuthRepository(
           gh<_i441.BaseAuthRemoteDataSource>(),
           gh<_i615.NetworkInfo>(),
         ));
-    gh.singleton<_i350.ClockinUsecase>(
-        () => _i350.ClockinUsecase(gh<_i171.BaseDashboardReposetry>()));
-    gh.singleton<_i938.ClockOutUsecase>(
-        () => _i938.ClockOutUsecase(gh<_i171.BaseDashboardReposetry>()));
-    gh.singleton<_i615.GetProfileUsecase>(
-        () => _i615.GetProfileUsecase(gh<_i283.BaseAuthReposetry>()));
     gh.singleton<_i18.LoginUseCase>(
         () => _i18.LoginUseCase(gh<_i283.BaseAuthReposetry>()));
-    gh.factory<_i1067.ClockInCubit>(() => _i1067.ClockInCubit(
+    gh.singleton<_i615.GetProfileUsecase>(
+        () => _i615.GetProfileUsecase(gh<_i283.BaseAuthReposetry>()));
+    gh.factory<_i883.HomeCubit>(() => _i883.HomeCubit(
           repository: gh<_i233.HomeRepository>(),
           locationService: gh<_i984.LocationService>(),
         ));
-    gh.factory<_i883.HomeCubit>(() => _i883.HomeCubit(
+    gh.factory<_i1067.ClockInCubit>(() => _i1067.ClockInCubit(
           repository: gh<_i233.HomeRepository>(),
           locationService: gh<_i984.LocationService>(),
         ));
