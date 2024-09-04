f10876e68f77cbaa8a1d0a1e2787b7b44fda95f6
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-09-04 10:02:12 +0300

    Change the map and to map methods

--- lib/features/dashboard/data/models/clock_models/clock_request.dart
+++ lib/features/dashboard/data/models/clock_models/clock_request.dart
@@ -19,39 +19,42 @@ class ClockRequest {
   });
 
   Map<String, dynamic> toMap() {
-    final dateFormat =
-        DateFormat('yyyy-MM-dd HH:mm:ss'); // Define the date format
+    final dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
 
     return <String, dynamic>{
       'latitude': latitude,
       'longitude': longitude,
       'location_type':
           isFromSite == true ? LocationType.site.name : LocationType.home.name,
-      'clock_in': clockIn != null
-          ? dateFormat.format(clockIn!.toUtc())
-          : null, // Format clockIn
-      'clock_out': clockOut != null
-          ? dateFormat.format(clockOut!.toUtc())
-          : null, // Format clockOut
+      'clock_in': clockIn != null ? dateFormat.format(clockIn!.toUtc()) : null,
+      'clock_out':
+          clockOut != null ? dateFormat.format(clockOut!.toUtc()) : null,
     };
   }
 
+  // Create object from map (like from an API response or local storage)
   factory ClockRequest.fromMap(Map<String, dynamic> map) {
+    final dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
+
     return ClockRequest(
       latitude: map['latitude'] != null ? map['latitude'] as double : null,
       longitude: map['longitude'] != null ? map['longitude'] as double : null,
-      isFromSite: map['isFromSite'] != null ? map['isFromSite'] as bool : null,
+      isFromSite: map['location_type'] != null
+          ? map['location_type'] as String == LocationType.site.name
+          : false,
       clockIn: map['clock_in'] != null
-          ? DateTime.fromMillisecondsSinceEpoch(map['clock_in'] as int)
+          ? dateFormat.parseUtc(map['clock_in'] as String)
           : null,
       clockOut: map['clock_out'] != null
-          ? DateTime.fromMillisecondsSinceEpoch(map['clock_out'] as int)
+          ? dateFormat.parseUtc(map['clock_out'] as String)
           : null,
     );
   }
 
+  // Convert object to JSON string
   String toJson() => json.encode(toMap());
 
+  // Create object from JSON string
   factory ClockRequest.fromJson(String source) =>
       ClockRequest.fromMap(json.decode(source) as Map<String, dynamic>);
 }
