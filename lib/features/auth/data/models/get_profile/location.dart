1b859177e608c57c5d9aae04f00fea5a41b6dea0
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-09-18 13:46:58 +0300

    Make sure we receive the correct time of each location

--- lib/features/auth/data/models/get_profile/location.dart
+++ lib/features/auth/data/models/get_profile/location.dart
@@ -1,18 +1,30 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:intl/intl.dart';
+
 class Location {
   final int? locationId;
   final String? locationName;
+  final DateTime? locationStartTime;
+  final DateTime? locationEndTime;
   Location({
     this.locationId,
     this.locationName,
+    this.locationEndTime,
+    this.locationStartTime,
   });
 
   Map<String, dynamic> toMap() {
+    final timeFormat = DateFormat("HH:mm:ss");
     return <String, dynamic>{
       'location_id': locationId,
       'location_name': locationName,
+      'location_start_time': locationStartTime != null
+          ? timeFormat.format(locationStartTime!)
+          : null,
+      'location_end_time':
+          locationEndTime != null ? timeFormat.format(locationEndTime!) : null,
     };
   }
 
@@ -21,6 +33,12 @@ class Location {
       locationId: map['location_id'] != null ? map['location_id'] as int : null,
       locationName:
           map['location_name'] != null ? map['location_name'] as String : null,
+      locationStartTime: map['location_start_time'] != null
+          ? Location()._parseTimeString(map['location_start_time'] as String)
+          : null,
+      locationEndTime: map['location_end_time'] != null
+          ? Location()._parseTimeString(map['location_end_time'] as String)
+          : null,
     );
   }
 
@@ -28,4 +46,13 @@ class Location {
 
   factory Location.fromJson(String source) =>
       Location.fromMap(json.decode(source) as Map<String, dynamic>);
+  DateTime? _parseTimeString(String? timeString) {
+    if (timeString == null) return null;
+    final timeFormat = DateFormat("HH:mm:ss");
+    DateTime parsedTime = timeFormat.parse(timeString);
+    DateTime now = DateTime.now();
+    DateTime completeDateTime = DateTime(now.year, now.month, now.day,
+        parsedTime.hour, parsedTime.minute, parsedTime.second);
+    return completeDateTime;
+  }
 }
