b413f3423c927a8631fa522719f5b3d1ad937208
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-09-20 19:57:42 +0300

    Setup the enviroment of the notification settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pyramakerz_atendnace/core/app-router/app_router.dart';
 import 'package:pyramakerz_atendnace/core/asset_manger/app_string.dart';
 import 'package:pyramakerz_atendnace/core/di/dependency_config.dart';
+import 'package:pyramakerz_atendnace/core/services/notifications_service.dart';
 import 'package:pyramakerz_atendnace/my_blocs_provider.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await configureDependencies();
 
   initializeTimeZones();
+  await NotificationsService.init();
+  var initialNotification =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  if (initialNotification?.didNotificationLaunchApp == true) {
+    Future.delayed(const Duration(seconds: 1), () {});
+  }
+
   runApp(const MyApp());
 }
 
